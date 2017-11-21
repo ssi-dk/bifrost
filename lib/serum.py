2f52e218a02278906a218881b7921ac7dd01837a
Kim Ng <kimleeng@gmail.com>
2017-11-21 13:37:37 +0100

    debug

--- lib/serum.py
+++ lib/serum.py
@@ -9,9 +9,6 @@ import vcf
 import pkg_resources
 
 configfile = pkg_resources.resource_string(__name__, "../config/config.yaml")
-print(configfile)
-print(os.cwd())
-print(__file__)
 yaml = ruamel.yaml.YAML(typ='safe')
 yaml.default_flow_style = False
 with open(configfile, "r") as yaml_stream:
