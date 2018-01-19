29011b53c304c7434b79a9c4d2920ffb437dc909
Kim Ng <kimleeng@gmail.com>
2018-01-19 10:37:28 +0100

    debug

--- lib/serum.py
+++ lib/serum.py
@@ -136,6 +136,7 @@ def start_initialized_samples(run_dir=".", run_status="run_status.csv"):
                     sample_info = yaml.load(yaml_stream)
                 if sample_info['sample']['status'] == 'initialized':
                     sample_info['sample']['status'] = 'starting'
+                    if os.path.isfile(os.path.join(directory, "cmd.sh")):
                     with open(os.path.join(directory, "cmd.sh"), "r") as sample_cmd:
                         run_cmd.write("cd {}\n".format(directory))
                         run_cmd.write(sample_cmd.readlines())
