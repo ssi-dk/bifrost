6cf799c33ea86556b35d68b28f14dcb2ca2a5b22
Martín Basterrechea <martinbaste@gmail.com>
2018-10-04 14:33:19 +0200

    null

--- reporter/reporter.py
+++ reporter/reporter.py
@@ -633,7 +633,7 @@ def update_test_table(species_list, group_list, run_name):
             if str(value).startswith("fail") or str(value).startswith("undefined") \
             or value == "supplying lab" or value =="core facility":
                 td = html.Td(str(value), className="cell red")
-            elif str(value).startswith("KeyError") or (column.endswith("QC") and pd.isna(value)):
+            elif str(value).startswith("KeyError") or (column.endswith("QC") and pd.isnull(value)):
                 td = html.Td(str(value), className="cell yellow")
             else:
                 td = html.Td(str(value), className="cell")
