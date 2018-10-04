905619003b1c8d0d8c2202ba34e0b89ef6185c42
Martín Basterrechea <martinbaste@gmail.com>
2018-10-04 14:36:20 +0200

    debug

--- reporter/reporter.py
+++ reporter/reporter.py
@@ -630,11 +630,11 @@ def update_test_table(species_list, group_list, run_name):
     for sample in samples_df.iterrows():
         row = []
         for value, column in zip(sample[1][columns], columns):
+            print(value)
+            print(type(value))
             if str(value).startswith("fail") or str(value).startswith("undefined") \
             or value == "supplying lab" or value =="core facility":
                 td = html.Td(str(value), className="cell red")
-            print(value)
-            print(type(value))
             elif str(value).startswith("KeyError") or (column.endswith("QC") and pd.isnull(value)):
                 td = html.Td(str(value), className="cell yellow")
             else:
