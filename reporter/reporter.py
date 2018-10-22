9f1f9fdd50ae3543b88c750186b4a97767638fab
Martín Basterrechea <martinbaste@gmail.com>
2018-10-22 11:31:30 +0200

    reporter link back to runner

--- reporter/reporter.py
+++ reporter/reporter.py
@@ -15,6 +15,7 @@ import dash_scroll_up
 
 import flask  #used for the image server
 
+import keys
 
 import components.mongo_interface
 import import_data
@@ -68,6 +69,7 @@ app.layout = html.Div([
         html.Div(dt.DataTable(rows=[{}], editable=False), style={"display": "none"}),
         html.H1("SerumQC REPORT"),
         html.H2("Loading...", id="run-name"),
+        html.Div(id="report-link"),
         html.H3(html.A("Wiki is here", href="https://teams.microsoft.com/l/channel/19%3a7b0b9a088602419e9f84630bacc84c2e%40thread.skype/tab%3a%3a9098abb1-75f5-410a-9011-87db7d42f3c2?label=Wiki&groupId=16852743-838a-400e-921d-6c50cc495b2f&tenantId=d0155445-8a4c-4780-9c13-33c78f22890e")),
         dcc.Location(id="url", refresh=False),
         html.Div(html_table([["run_name", ""]]), id="run-table"),
@@ -238,6 +240,16 @@ def update_run_button(run):
         return "/" + run
     else:
         return "/"
+
+@app.callback(
+    Output("report-link", "children"),
+    [Input("run-name", "children")]
+)
+def update_run_name(run_name):
+    if run_name == "" or run_name == "Not found":
+        return None
+    else:
+        return html.H3(html.A("Link to Run Status", href="{}/{}".format(keys.run_checker_url, run_name)))
 
 @app.callback(
     Output("report-count", "children"),
