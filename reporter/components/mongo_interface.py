24310566454dfcc9a1f2ea0b4a53bce1ecc6eeda
Martín Basterrechea <martinbaste@gmail.com>
2019-01-08 10:57:30 +0100

    read from new stamp structure in reporter mongo AB#62

--- reporter/components/mongo_interface.py
+++ reporter/components/mongo_interface.py
@@ -56,7 +56,7 @@ def get_species_plot_data(species_list, id_list, component="assemblatron"):
                     "_id": {"$in": routine_list, "$nin": id_list},
                     "properties.species": {"$in": species_list},
                     # NOTE change stamp structure from list to dict to keep only latest.
-                    "stamps" : {"$elemMatch": {"name": "ssi_stamper", "value": "pass:OK"}}
+                    "stamps.ssi_stamper" : "pass:OK"
                 }
             },
             {
