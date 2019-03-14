fc02487abd0027066ef7212e73cd257d99da540d
Martín Basterrechea <martinbaste@gmail.com>
2019-03-14 11:01:25 +0100

    move text how it works

--- reporter/components/admin.py
+++ reporter/components/admin.py
@@ -6,22 +6,15 @@ import dash_core_components as dcc
 def html_qc_expert_form():
     if ("REPORTER_ADMIN" in os.environ and os.environ["REPORTER_ADMIN"] == "True"):
         return html.Div([
-            html.Div(html.H5("Submit Supplying Lab Feedback:",
-                             className="nomargin"), className="six columns"),
-            html.Div(dcc.Input(id="qc-user-1", placeholder="Your initials (e.g. MBAS)",
-                               value="", type="text"), className="three columns"),
-            html.Div(html.Button("Submit QC values",
-                                 id="feedback-button"), className="three columns")
-        ], className="row mb-1")
-    else:
-        return None
-
-
-def sample_radio_feedback(sample, n_sample):
-    if ("REPORTER_ADMIN" in os.environ and os.environ["REPORTER_ADMIN"] == "True"):
-        return (html.Div([
             html.Div([
-                html.Label("Supplying Lab Feedback:"),
+                html.Div(html.H5("Submit Supplying Lab Feedback:",
+                                className="nomargin"), className="six columns"),
+                html.Div(dcc.Input(id="qc-user-1", placeholder="Your initials (e.g. MBAS)",
+                                value="", type="text"), className="three columns"),
+                html.Div(html.Button("Submit QC values",
+                                    id="feedback-button"), className="three columns")
+            ], className="row"),
+            html.Div([
                 html.Details([
                     html.Summary("How it works (click to show)"),
                     html.P("""If you want to send a sample to resequence,
@@ -31,6 +24,19 @@ def sample_radio_feedback(sample, n_sample):
                     with the "Supplying lab" warning and their data will
                     be used in the future to adjust QC thresholds.""")
                 ])
+            ], className="row mb-1")
+
+        ])
+        
+    else:
+        return None
+
+
+def sample_radio_feedback(sample, n_sample):
+    if ("REPORTER_ADMIN" in os.environ and os.environ["REPORTER_ADMIN"] == "True"):
+        return (html.Div([
+            html.Div([
+                html.Label("Supplying Lab Feedback:"),
             ],className="three columns"),
             html.Div(
                 dcc.RadioItems(
