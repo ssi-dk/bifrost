0f824a52fd3ae6e266ad383e8f7cbd10002b842b
Martín Basterrechea <martinbaste@gmail.com>
2019-03-18 14:18:09 +0100

    shorter lines in datahandling

--- lib/bifrostlib/bifrostlib/datahandling.py
+++ lib/bifrostlib/bifrostlib/datahandling.py
@@ -1,17 +1,20 @@
 import pkg_resources
 import ruamel.yaml
 from bifrostlib import mongo_interface
-import bson
+import bson.objectid.ObjectId as ObjectId
 import os
 
 
-bson.objectid.ObjectId.yaml_tag = u'!bson.objectid.ObjectId'
-bson.objectid.ObjectId.to_yaml = classmethod(lambda cls, representer, node: representer.represent_scalar(cls.yaml_tag, u'{}'.format(node)))
-bson.objectid.ObjectId.from_yaml = classmethod(lambda cls, constructor, node: cls(node.value))
+ObjectId.yaml_tag = u'!bson.objectid.ObjectId'
+ObjectId.to_yaml = classmethod(
+    lambda cls, representer, node: representer.represent_scalar(
+        cls.yaml_tag, u'{}'.format(node)))
+ObjectId.from_yaml = classmethod(
+    lambda cls, constructor, node: cls(node.value))
 
 yaml = ruamel.yaml.YAML(typ="safe")
 yaml.default_flow_style = False
-yaml.register_class(bson.objectid.ObjectId)
+yaml.register_class(ObjectId)
 
 
 def connect_or_initialize_and_connect(config):
@@ -63,7 +66,8 @@ def load_component(file_yaml):
 
 def save_sample(sample_dict, file_yaml):
     config = load_config()
-    if len(file_yaml.split("/")) >= 2 and os.path.isdir("/".join(file_yaml.split("/")[:-1])) is False:
+    if (len(file_yaml.split("/")) >= 2 and
+            os.path.isdir("/".join(file_yaml.split("/")[:-1])) is False):
         os.mkdir("/".join(file_yaml.split("/")[:-1]))
     if config["use_mongodb"]:
         sample_dict = mongo_interface.dump_sample_info(sample_dict)
@@ -84,10 +88,12 @@ def load_sample(file_yaml):
 
 def save_sample_component(sample_component_dict, file_yaml):
     config = load_config()
-    if len(file_yaml.split("/")) >= 2 and os.path.isdir("/".join(file_yaml.split("/")[:-1])) is False:
+    if (len(file_yaml.split("/")) >= 2 and
+            os.path.isdir("/".join(file_yaml.split("/")[:-1])) is False):
         os.mkdir("/".join(file_yaml.split("/")[:-1]))
     if config["use_mongodb"]:
-        sample_component_dict = mongo_interface.dump_sample_component_info(sample_component_dict)
+        sample_component_dict = mongo_interface.dump_sample_component_info(
+            sample_component_dict)
     with open(file_yaml, "w") as file_handle:
         yaml.dump(sample_component_dict, file_handle)
 
@@ -145,7 +151,8 @@ def read_buffer(file_path):
 def get_mlst_species_DB(file_yaml):
     with open(file_yaml, "r") as file_handle:
         sample = yaml.load(file_handle)
-        return mongo_interface.query_mlst_species(sample["properties"].get("species", None))
+        species = sample["properties"].get("species", None)
+        return mongo_interface.query_mlst_species(species)
 
 
 def load_species(ncbi_species):
@@ -156,27 +163,33 @@ def get_ncbi_species(species_entry):
     return mongo_interface.query_ncbi_species(species_entry)
 
 
-def datadump_template(data_dict, component_folder, file_path, extraction_callback):
+def datadump_template(data_dict, component_folder,
+                      file_path, extraction_callback):
     file_path_key = file_path.replace(".", "_")
     if os.path.isfile(os.path.join(component_folder, file_path)):
         data_dict["results"][file_path_key] = {}
         try:
-            data_dict = extraction_callback(os.path.join(component_folder, file_path), file_path_key, data_dict)
+            data_dict = extraction_callback(os.path.join(component_folder,
+                                                         file_path),
+                                            file_path_key, data_dict)
         except Exception as e:
             print(file_path, e)
             data_dict["results"][file_path_key]["status"] = "datadumper error"
         return data_dict
 
+
 def load_run_from_db(name=None):
     return mongo_interface.load_run(name)
 
+
 def load_sample_from_db(sample_id):
-    return mongo_interface.load_sample(bson.objectid.ObjectId(sample_id))
+    return mongo_interface.load_sample(ObjectId(sample_id))
 
 
 def load_samples_from_db(sample_ids):
-    sample_ids = [bson.objectid.ObjectId(id) for id in sample_ids]
+    sample_ids = [ObjectId(id) for id in sample_ids]
     return mongo_interface.load_samples(sample_ids)
+
 
 def save_sample_to_db(sample):
     return mongo_interface.dump_sample_info(sample)
@@ -191,14 +204,15 @@ def save_component_to_db(component):
 
 
 def load_last_sample_component(sample_id, component_name):
-    return mongo_interface.load_last_sample_component(bson.objectid.ObjectId(sample_id), component_name)
+    return mongo_interface.load_last_sample_component(ObjectId(sample_id),
+                                                      component_name)
 
 
 def load_samples_from_runs(run_ids=None, names=None):
     if run_ids is not None:
         if type(run_ids) == str:
             run_ids = [run_ids]
-        run_ids = [bson.objectid.ObjectId(id) for id in run_ids]
+        run_ids = [ObjectId(id) for id in run_ids]
     res = mongo_interface.load_samples_from_runs(run_ids, names)
     sample_ids = set()  # avoid dupes
     for run in res:
@@ -217,15 +231,21 @@ def delete_sample(sample_id):
     """
     delete_sample_component(sample_id=sample_id)
     delete_sample_from_runs(sample_id=sample_id)
-    deleted = mongo_interface.delete_sample(sample_id=sample_id)
+    deleted = mongo_interface.delete_sample(sample_id=ObjectId(sample_id))
     return deleted
 
 
 def delete_sample_component(s_c_id=None, sample_id=None):
+    if s_c_id is not None:
+        s_c_id = ObjectId(s_c_id)
+    if sample_id is not None:
+        sample_id = ObjectId(sample_id)
     return mongo_interface.delete_sample_component(s_c_id, sample_id)
 
 
 def delete_sample_from_runs(sample_id=None):
+    if sample_id is not None:
+        sample_id = ObjectId(sample_id)
     return mongo_interface.delete_sample_from_runs(sample_id)
 
 
