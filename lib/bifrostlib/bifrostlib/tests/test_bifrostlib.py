f744fd16c93100d057f7067ea3f14d3f4481d002
Martín Basterrechea <martinbaste@gmail.com>
2019-03-19 15:09:50 +0100

    mongomock patch

--- lib/bifrostlib/bifrostlib/tests/test_bifrostlib.py
+++ lib/bifrostlib/bifrostlib/tests/test_bifrostlib.py
@@ -6,7 +6,7 @@ def test_bifrostlib():
     assert datahandling.test() == u"This is the bifrost lib"
 
 
-@mongomock.patch(servers=(('server.example.com', 27017),))
+@mongomock.patch(('server.example.com', 27017))
 def test_post_sample():
     sample = {"name": "test_sample"}
     sample_db = datahandling.save_sample_to_db(sample)
