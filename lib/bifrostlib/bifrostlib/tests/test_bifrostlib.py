38510e0ae82ebec2470d0a77c15d1dae8541913d
Martín Basterrechea <martinbaste@gmail.com>
2019-03-19 14:46:42 +0100

    add save sample test

--- lib/bifrostlib/bifrostlib/tests/test_bifrostlib.py
+++ lib/bifrostlib/bifrostlib/tests/test_bifrostlib.py
@@ -1,5 +1,13 @@
+import mongomock
 from bifrostlib import datahandling
 
 
 def test_bifrostlib():
     assert datahandling.test() == u"This is the bifrost lib"
+
+
+def test_post_sample():
+    sample = {"name": "test_sample"}
+    sample_db = datahandling.save_sample_to_db(sample)
+    sample["_id"] = sample_db["_id"]
+    assert sample == sample_db
