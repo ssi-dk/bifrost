5b714e29d61e36e5414e9b24fc1cf147a83a58b3
Kim Ng <kimleeng@gmail.com>
2019-08-07 09:49:23 +0200

    lost results adding back in

--- components/cge_mlst/datadump.py
+++ components/cge_mlst/datadump.py
@@ -16,6 +16,7 @@ def script__datadump(folder, sample_file, component_file, sample_component_file,
     species = db_sample["properties"]["species"]
 
     db_sample_component["summary"] = {"db": [], "strain": [], "alleles": [], "component": {"id": db_component["_id"], "date": datetime.datetime.utcnow()}}
+    db_sample_component["results"] = {}
 
     mlst_species = db_component["mlst_species_mapping"][species]
     for mlst_entry in mlst_species:
