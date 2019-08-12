2a29f2e83faf6824beb1f558f3a0809cc5036d37
Kim Ng <kimleeng@gmail.com>
2019-08-12 14:09:32 +0200

    debug

--- components/cge_mlst/scripts/run_cge_mlst.py
+++ components/cge_mlst/scripts/run_cge_mlst.py
@@ -26,14 +26,13 @@ def script__run_cge_mlst(input, output, sample_file, component_file, folder, log
             datahandling.log(log_out, "cge mlst species: {}\n".format(species))
             subprocess.Popen("touch " + folder + "/no_mlst_species_DB").communicate()
         else:
-            mlst_database_path = db_component["mlst_database_path"]
             mlst_species = db_component["mlst_species_mapping"][species]
             data_dict = {}
             for mlst_entry in mlst_species:
                 mlst_entry_path = folder + "/" + mlst_entry
                 datahandling.log(log_out, "mlst {} on species: {}\n".format(mlst_entry, species))
                 subprocess.Popen("if [ -d \"{}\" ]; then rm -r {}; fi".format(mlst_entry_path, mlst_entry_path), shell=True).communicate()
-                subprocess.Popen("mkdir {}; mlst.py -x -matrix -s {} -p {} -mp kma -i {} {} -o {} 1> {} 2> {}".format(mlst_entry_path, mlst_entry, mlst_database_path, reads[0], reads[1], mlst_entry_path, log_out, log_err), shell=True).communicate()
+                subprocess.Popen("mkdir {}; mlst.py -x -matrix -s {} -p {} -mp kma -i {} {} -o {} 1> {} 2> {}".format(mlst_entry_path, mlst_entry, database_path, reads[0], reads[1], mlst_entry_path, log_out, log_err), shell=True).communicate()
                 data_dict[mlst_entry] = datahandling.load_yaml(mlst_entry + "/data.json")
             datahandling.save_yaml(data_dict, output_file)
 
