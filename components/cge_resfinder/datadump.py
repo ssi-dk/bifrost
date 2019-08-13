7aaf44733f429a4c3482ee8240d08ba41fe87a1e
Kim Ng <kimleeng@gmail.com>
2019-08-13 13:45:10 +0200

    testing file save

--- components/cge_resfinder/datadump.py
+++ components/cge_resfinder/datadump.py
@@ -38,6 +38,9 @@ def script__datadump(output, folder, sample_file, component_file, sample_compone
 
         datahandling.log(log_out, "Started {}\n".format(this_function_name))
 
+        # Save files to DB
+        datahandling.save_files_to_db(["cge_resfinder/results.txt", "cge_resfinder/results_tab.txt"], sample_component_id=db_sample_component["_id"])
+
         # Initialization of values, summary and reporter are also saved into the sample
         db_sample_component["summary"] = {"component": {"id": db_component["_id"], "date": datetime.datetime.utcnow()}}
         db_sample_component["results"] = {}
