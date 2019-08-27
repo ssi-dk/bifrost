b375455d47186f32eed0ec44155ef7e30c5e3329
Kim Ng <kimleeng@gmail.com>
2019-08-27 13:42:41 +0200

    debug

--- lib/bifrostlib/bifrostlib/mongo_interface.py
+++ lib/bifrostlib/bifrostlib/mongo_interface.py
@@ -133,12 +133,13 @@ def dump_component_info(data_dict):
         )
     else:
         data_dict["metadata"]["created_at"] = now
-        component = get_components(component_names=[data_dict["name"]], component_versions=[data_dict["version"]])
-        if len(component) == 0:
-            result = components_db.insert_one(data_dict)
-            data_dict["_id"] = result.inserted_id
-        if len(component) == 1:
-            data_dict["_id"] = component[0]["_id"]
+
+        data_dict = components_db.find_one_and_update(
+            filter={"name": data_dict["name"], "version": data_dict["version"]},
+            update={"$setOnInsert": data_dict},
+            return_document=pymongo.ReturnDocument.AFTER,
+            upsert=True
+        )
 
     return data_dict
 
