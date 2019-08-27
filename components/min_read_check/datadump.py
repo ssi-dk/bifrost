2e8953a8b80fb22911c4fe1eb7efff4eee06aaee
Kim Ng <kimleeng@gmail.com>
2019-08-27 09:20:57 +0200

    test

--- components/min_read_check/datadump.py
+++ components/min_read_check/datadump.py
@@ -8,14 +8,18 @@ import os
 from bifrostlib import datahandling
 
 def extract_has_min_num_of_reads(datadumpObj):
-    import re
-    summary, results = datadumpObj.load_summary_and_results()
-    file_path = os.path.join(datadumpObj.load_component_name(), "has_min_num_of_reads")
-    buffer = datahandling.read_buffer(file_path)
-    results[file_path] = {}
-    results[file_path]["min_read_num"] = int(re.search("min_read_num:\s*([0-9]+)", buffer, re.MULTILINE).group(1))
-    summary["has_min_num_of_reads"] = True
-    return (summary, results)
+    results = {}
+    try:
+        import re
+        summary, results = datadumpObj.load_summary_and_results()
+        file_path = os.path.join(datadumpObj.load_component_name(), "has_min_num_of_reads")
+        buffer = datahandling.read_buffer(file_path)
+        results[file_path] = {}
+        results[file_path]["min_read_num"] = int(re.search("min_read_num:\s*([0-9]+)", buffer, re.MULTILINE).group(1))
+        summary["has_min_num_of_reads"] = True
+        return (summary, results)
+    except Exception:
+        print(results)
 
 def script__datadump(sample_file, component_file, sample_component_file, log):
     datadumpObj = datahandling.DatadumpSampleComponentObj(sample_file, component_file, sample_component_file, log)
