a861803fe80f2a65a0791aaf7ee3100525c2a002
Martín Basterrechea <martinbaste@gmail.com>
2019-08-29 14:58:03 +0200

    ariba mlst done

--- components/ariba_mlst/scripts/rule__run_ariba_mlst.py
+++ components/ariba_mlst/scripts/rule__run_ariba_mlst.py
@@ -10,6 +10,7 @@ def rule__run_ariba_mlst(input, output, sampleComponentObj, log):
         this_function_name = sys._getframe().f_code.co_name
         sample_db, component_db = sampleComponentObj.start_rule(
             this_function_name, log=log)
+        component_name = sampleComponentObj.get_component_name()
 
         sampleComponentObj.write_log_out(log, "Started {}\n".format(this_function_name))
         # Variables being used
@@ -21,27 +22,28 @@ def rule__run_ariba_mlst(input, output, sampleComponentObj, log):
         # Code to run
         if species not in component_db["options"]["mlst_species_mapping"]:
             sampleComponentObj.write_log_out(log, "cge mlst species: {}\n".format(species))
-            subprocess.Popen("touch " + folder + "/no_mlst_species_DB").communicate()
+            sampleComponentObj.rule_run_cmd(
+                "touch " + component_name + "/no_mlst_species_DB", log)
         else:
             mlst_species = component_db["options"]["mlst_species_mapping"][species]
             data_dict = {}
             for mlst_entry in mlst_species:
                 data_dict[mlst_entry] = {}
-                mlst_entry_path = folder + "/" + mlst_entry
+                mlst_entry_path = component_name + "/" + mlst_entry
                 mlst_database_path = os.path.join(database_path, mlst_entry, "ref_db")
                 sampleComponentObj.write_log_out(log, "mlst {} on species: {}\n".format(mlst_entry, species))
+
                 command = "if [ -d \"{}\" ]; then rm -r {}; fi".format(mlst_entry_path, mlst_entry_path)
-                sampleComponentObj.write_log_out(log, "Running:{}".format(command))
-                subprocess.Popen(command, shell=True).communicate()
+                sampleComponentObj.rule_run_cmd(command, log)
                 command = "ariba run --force {} {} {} {} 1> {} 2> {}".format(mlst_database_path, reads[0], reads[1], mlst_entry_path, log_out, log_err)
-                sampleComponentObj.write_log_out(log, "Running:{}".format(command))
-                subprocess.Popen(command, shell=True).communicate()
+                sampleComponentObj.rule_run_cmd(command, log)
+                
                 data_dict[mlst_entry]["report"] = pandas.read_csv(os.path.join(mlst_entry_path, "mlst_report.tsv"), sep="\t").to_dict(orient="records")[0]
                 data_dict[mlst_entry]["report_details"] = pandas.read_csv(os.path.join(mlst_entry_path, "mlst_report.details.tsv"), sep="\t", index_col="gene").to_dict(orient="index")
             datahandling.save_yaml(data_dict, output_file)
     except Exception:
         sampleComponentObj.write_log_out(log, "Exception in {}\n".format(this_function_name))
-        datahandling.write_log(log_err, str(traceback.format_exc()))
+        sampleComponentObj.write_log_err(log, str(traceback.format_exc()))
 
     finally:
         sampleComponentObj.write_log_out(log, "Done {}\n".format(this_function_name))
