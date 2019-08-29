d0dad2f4a17c876a8c01a270edfad7ee3b9d3f8b
Kim Ng <kimleeng@gmail.com>
2019-08-29 08:45:39 +0200

    debug

--- lib/bifrostlib/bifrostlib/datahandling.py
+++ lib/bifrostlib/bifrostlib/datahandling.py
@@ -213,7 +213,7 @@ def load_component(file_yaml):
 def save_sample(sample_db):
     #TODO: This writing to a file is temporary and should be removed with bifrost.smk refactoring
     if "path" in sample_db:
-        with open(os.path.join(sample_db["path"],"sample.yaml")) as file_handle:
+        with open(os.path.join(sample_db["path"], "sample.yaml"), "w") as file_handle:
             yaml.dump(sample_db, file_handle)
 
     return mongo_interface.dump_sample_info(sample_db)
@@ -243,7 +243,7 @@ def save_sample_component(sample_component_db):
     #TODO: This writing to a file is temporary and should be removed with bifrost.smk refactoring
     sample_db = get_sample(sample_component_db["sample"]["_id"])
     if "path" in sample_db:
-        with open(os.path.join(sample_db["path"], "{}__{}.yaml".format(sample_component_db["sample"]["name"], sample_component_db["component"]["name"]))) as file_handle:
+        with open(os.path.join(sample_db["path"], "{}__{}.yaml".format(sample_component_db["sample"]["name"], sample_component_db["component"]["name"])), "w") as file_handle:
             yaml.dump(sample_component_db, file_handle)
 
     return mongo_interface.dump_sample_component_info(sample_component_db)
