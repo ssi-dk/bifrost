b0209f7d88b38477f52bdd971372bde7bfcfb560
Kim Ng <kimleeng@gmail.com>
2019-08-30 13:24:26 +0200

    debug

--- components/min_read_check/datadump.py
+++ components/min_read_check/datadump.py
@@ -3,7 +3,7 @@ from bifrostlib import datahandling
 
 def extract_has_min_num_of_reads(sampleComponentObj):
     import re
-    summary, results, file_path, key = sampleComponentObj.start_data_extraction("data.yaml")
+    summary, results, file_path, key = sampleComponentObj.start_data_extraction("has_min_num_of_reads")
     buffer = datahandling.read_buffer(file_path)
     results[key]["min_read_num"] = int(re.search("min_read_num:\s*([0-9]+)", buffer, re.MULTILINE).group(1))
     summary["has_min_num_of_reads"] = True
