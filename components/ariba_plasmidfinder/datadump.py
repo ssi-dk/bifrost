2ce5efa7f4dead64234b86b11451b9663d933e2e
Kim Ng <kimleeng@gmail.com>
2019-09-09 11:03:48 +0200

    Adding plasmidfinder and virulencefinder

---
+++ components/ariba_plasmidfinder/datadump.py
@@ -0,0 +1,73 @@
+from bifrostlib import datahandling
+
+
+def extract_ariba_plasmidfinder_data(sampleComponentObj):
+    import pandas
+    summary, results, file_path, key = sampleComponentObj.start_data_extraction("plasmid/report.tsv")
+    # all the rows are turned into a subelement of one gene, all subelements are expected to be the same size
+    #- Sub Function start --------------------------------------------------------------------------
+    def snp_info_parser(row):
+        var_info = []
+        num_of_snp_elements = len(row["has_known_var"])
+        for i in range(num_of_snp_elements):
+            var_info.append({
+                "known_var": row["known_var"][i],
+                "var_type": row["var_type"][i],
+                "var_seq_type": row["var_seq_type"][i],
+                "known_var_change": row["known_var_change"][i],
+                "has_known_var": row["has_known_var"][i],
+                "ref_ctg_change": row["ref_ctg_change"][i],
+                "ref_ctg_effect": row["ref_ctg_effect"][i],
+                "ref_start": row["ref_start"][i],
+                "ref_end": row["ref_end"][i],
+                "ref_nt": row["ref_nt"][i],
+                "ctg_start": row["ctg_start"][i],
+                "ctg_end": row["ctg_end"][i],
+                "ctg_nt": row["ctg_nt"][i],
+                "smtls_total_depth": row["smtls_total_depth"][i],
+                "smtls_nts": row["smtls_nts"][i],
+                "smtls_nts_depth": row["smtls_nts_depth"][i]})
+        return var_info
+    #- Sub Function end ----------------------------------------------------------------------------
+    df = pandas.read_csv(file_path, sep="\t")
+    # These are all the columns which will be included in the final dataframe except for the collapsed column
+    grouped_df = df.groupby(["#ariba_ref_name", "ref_name", "gene", "var_only", "flag", "reads", "cluster", "ref_len", "ref_base_assembled", "pc_ident", "ctg", "ctg_len", "ctg_cov", "var_description", "free_text"])
+    # Turn the columns which are not part of the final dataframe into lists, some will have 1 entry some will have multiple
+    flattened_df = grouped_df.agg(list)
+    # Reset the indexing for formatting purposes otherwise you layer them
+    flattened_df = flattened_df.reset_index()
+    # Apply the function onto the columns that are in multiples and convert data accordingly and assign that to a new variable
+    flattened_df["var_info"] = flattened_df.apply(snp_info_parser, axis=1)
+    # Drop the columns used to make the combined value from the function
+    flattened_df = flattened_df.drop(columns=["known_var", "var_type", "var_seq_type", "known_var_change", "has_known_var", "ref_ctg_change", "ref_ctg_effect", "ref_start", "ref_end", "ref_nt", "ctg_start", "ctg_end", "ctg_nt", "smtls_total_depth", "smtls_nts", "smtls_nts_depth"])
+    # Set the reference id back to a variable for the dict to be in proper format
+    flattened_df = flattened_df.set_index("#ariba_ref_name")
+
+    results[key] = flattened_df.to_dict(orient="index")
+    return (summary, results)
+
+
+def generate_report(sampleComponentObj):
+    summary, results, file_path, key = sampleComponentObj.start_data_extraction()
+    key = sampleComponentObj.get_file_location_key("plasmid/report.tsv")
+    data = []
+    for gene in results[key]:
+        variant_count = 0
+        for variant in results[key][gene]["var_info"]:
+            variant_count = variant_count + 1
+        data.append({
+            "gene": gene,
+            "coverage": round(results[key][gene].get("ref_base_assembled", 0) / results[key][gene].get("ref_len", 1), 3),
+            "identity": results[key][gene]["pc_ident"],
+            "variants": variant_count
+        })
+    return data
+
+def datadump(sampleComponentObj, log):
+    sampleComponentObj.start_data_dump(log=log)
+    sampleComponentObj.run_data_dump_on_function(extract_ariba_plasmidfinder_data, log=log)
+    sampleComponentObj.end_data_dump(generate_report_function=generate_report, log=log)
+
+datadump(
+    snakemake.params.sampleComponentObj,
+    snakemake.log)
---
+++ components/ariba_plasmidfinder/pipeline.smk
@@ -0,0 +1,112 @@
+#- Templated section: start ------------------------------------------------------------------------
+import os
+from bifrostlib import datahandling
+
+configfile: "../config.yaml"  # Relative to run directory
+
+num_of_threads, memory_in_GB = config["threads"], config["memory"]
+bifrost_sampleComponentObj = datahandling.SampleComponentObj()
+sample_name, component_name, dockerfile, options, bifrost_resources = bifrost_sampleComponentObj.load(config["sample_id"], config["component_id"])
+
+singularity: dockerfile
+
+
+onsuccess:
+    bifrost_sampleComponentObj.success()
+
+
+onerror:
+    bifrost_sampleComponentObj.failure()
+
+
+rule all:
+    input:
+        # file is defined by datadump function
+        component_name + "/datadump_complete"
+
+
+rule setup:
+    output:
+        init_file = touch(
+            temp(component_name + "/initialized")),
+    params:
+        folder = component_name
+
+
+rule_name = "check_requirements"
+rule check_requirements:
+    message:
+        "Running step:" + rule_name
+    threads:
+        num_of_threads
+    resources:
+        memory_in_GB = memory_in_GB
+    log:
+        out_file = component_name + "/log/" + rule_name + ".out.log",
+        err_file = component_name + "/log/" + rule_name + ".err.log",
+    benchmark:
+        component_name + "/benchmarks/" + rule_name + ".benchmark"
+    input:
+        folder = rules.setup.output.init_file,
+    output:
+        check_file = component_name + "/requirements_met",
+    params:
+        bifrost_sampleComponentObj
+    run:
+        bifrost_sampleComponentObj.check_requirements()
+#- Templated section: end --------------------------------------------------------------------------
+
+#* Dynamic section: start **************************************************************************
+rule_name = "ariba_plasmidfinder"
+rule ariba_plasmidfinder:
+    # Static
+    message:
+        "Running step:" + rule_name
+    threads:
+        num_of_threads
+    resources:
+        memory_in_GB = memory_in_GB
+    log:
+        out_file = rules.setup.params.folder + "/log/" + rule_name + ".out.log",
+        err_file = rules.setup.params.folder + "/log/" + rule_name + ".err.log",
+    benchmark:
+        rules.setup.params.folder + "/benchmarks/" + rule_name + ".benchmark"
+    # Dynamic
+    input:
+        check_file = rules.check_requirements.output.check_file,
+        folder = rules.setup.output.init_file,
+        reads = bifrost_sampleComponentObj.get_reads()
+    output:
+        complete = rules.setup.params.folder + "/plasmid/report.tsv"
+    params:
+        sampleComponentObj = bifrost_sampleComponentObj
+    script:
+        os.path.join(os.path.dirname(workflow.snakefile), "scripts/rule__ariba_plasmidfinder.py")
+#* Dynamic section: end ****************************************************************************
+
+#- Templated section: start ------------------------------------------------------------------------
+rule_name = "datadump"
+rule datadump:
+    # Static
+    message:
+        "Running step:" + rule_name
+    threads:
+        num_of_threads
+    resources:
+        memory_in_GB = memory_in_GB
+    log:
+        out_file = component_name + "/log/" + rule_name + ".out.log",
+        err_file = component_name + "/log/" + rule_name + ".err.log",
+    benchmark:
+        component_name + "/benchmarks/" + rule_name + ".benchmark"
+    input:
+        #* Dynamic section: start ******************************************************************
+        rules.ariba_plasmidfinder.output.complete  # Needs to be output of final rule
+        #* Dynamic section: end ********************************************************************
+    output:
+        complete = rules.all.input
+    params:
+        sampleComponentObj = bifrost_sampleComponentObj
+    script:
+        os.path.join(os.path.dirname(workflow.snakefile), "datadump.py")
+#- Templated section: end --------------------------------------------------------------------------
