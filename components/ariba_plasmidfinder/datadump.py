32a428fdbc9a6fec12d1f43fac26c0a2bcf9d06b
Kim Ng <kimleeng@gmail.com>
2019-09-09 14:02:01 +0200

    debug

--- components/ariba_plasmidfinder/datadump.py
+++ components/ariba_plasmidfinder/datadump.py
@@ -12,9 +12,6 @@ def extract_ariba_plasmidfinder_data(sampleComponentObj):
         num_of_snp_elements = len(row["has_known_var"])
         for i in range(num_of_snp_elements):
             var_info.append({
-                "ref_base_assembled": row["ref_base_assembled"][i],
-                "pc_ident": row["pc_ident"][i],
-                "ctg": row["ctg"][i],
                 "ctg_len": row["ctg_len"][i],
                 "ctg_cov": row["ctg_cov"][i],
                 "known_var": row["known_var"][i],
@@ -37,7 +34,7 @@ def extract_ariba_plasmidfinder_data(sampleComponentObj):
     #- Sub Function end ----------------------------------------------------------------------------
     df = pandas.read_csv(file_path, sep="\t")
     # These are all the columns which will be included in the final dataframe except for the collapsed column
-    grouped_df = df.groupby(["#ariba_ref_name", "ref_name", "gene", "var_only", "flag", "reads", "cluster", "ref_len", "var_description", "free_text"])
+    grouped_df = df.groupby(["ctg", "#ariba_ref_name", "ref_name", "gene", "var_only", "flag", "reads", "cluster", "ref_len", "ref_base_assembled", "pc_ident", "var_description", "free_text"])
     # Turn the columns which are not part of the final dataframe into lists, some will have 1 entry some will have multiple
     flattened_df = grouped_df.agg(list)
     # Reset the indexing for formatting purposes otherwise you layer them
@@ -45,9 +42,9 @@ def extract_ariba_plasmidfinder_data(sampleComponentObj):
     # Apply the function onto the columns that are in multiples and convert data accordingly and assign that to a new variable
     flattened_df["var_info"] = flattened_df.apply(snp_info_parser, axis=1)
     # Drop the columns used to make the combined value from the function
-    flattened_df = flattened_df.drop(columns=["ref_base_assembled", "pc_ident", "ctg", "ctg_len", "ctg_cov", "known_var", "var_type", "var_seq_type", "known_var_change", "has_known_var", "ref_ctg_change", "ref_ctg_effect", "ref_start", "ref_end", "ref_nt", "ctg_start", "ctg_end", "ctg_nt", "smtls_total_depth", "smtls_nts", "smtls_nts_depth"])
+    flattened_df = flattened_df.drop(columns=["ctg_len", "ctg_cov", "known_var", "var_type", "var_seq_type", "known_var_change", "has_known_var", "ref_ctg_change", "ref_ctg_effect", "ref_start", "ref_end", "ref_nt", "ctg_start", "ctg_end", "ctg_nt", "smtls_total_depth", "smtls_nts", "smtls_nts_depth"])
     # Set the reference id back to a variable for the dict to be in proper format
-    flattened_df = flattened_df.set_index("#ariba_ref_name")
+    flattened_df = flattened_df.set_index("ctg")
 
     results[key] = flattened_df.to_dict(orient="index")
     return (summary, results)
