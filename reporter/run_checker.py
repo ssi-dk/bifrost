d146218aede4eda2fee999f0331c6d0627571a0f
Martín Basterrechea <martinbaste@gmail.com>
2019-10-08 13:35:30 +0200

    debug

--- reporter/run_checker.py
+++ reporter/run_checker.py
@@ -350,14 +350,15 @@ def rerun_components_button(button, table_data):
             sample_command += command
 
         if keys.rerun["grid"] == "slurm":
-            print(sample_command)
+            process_command = 'sbatch --mem={memory}G -p {priority} -c {threads} '
+                '-t {walltime} -J "bifrost_{sample_name}" --wrap'
+                ' "{command}"').format(
+                **keys.rerun,
+                sample_name=sample_name,
+                command=sample_command)
+            print(process_command)
             process = subprocess.Popen(
-                ('sbatch --mem={memory}G -p {priority} -c {threads} '
-                    '-t {walltime} -J "bifrost_{sample_name}" --wrap'
-                    ' "{command}"').format(
-                        **keys.rerun,
-                        sample_name=sample_name,
-                        command=sample_command),
+                (process_command,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
                 shell=True,
