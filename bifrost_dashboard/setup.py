eb093820bde4caa240ccdfb20a94239349b2677f
Martín Basterrechea <martinbaste@gmail.com>
2020-06-18 15:56:55 +0200

    typo in setup

--- bifrost_dashboard/setup.py
+++ bifrost_dashboard/setup.py
@@ -14,7 +14,7 @@ setup(
     include_package_data=True,
     install_requires=[
         # 'pymongo',
-        'pandas'
+        'pandas',
         'dash',
         'bifrostapi',
         'Flask-Caching',
