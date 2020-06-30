8b74e693d84d6080ec9d938bf1f6eb04b9d17edb
Kim Ng <kimleeng@gmail.com>
2020-06-30 14:29:30 +0200

    2.0.6

--- lib/bifrostlib/setup.py
+++ lib/bifrostlib/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrostlib',
-    version='2.0.5',
+    version='2.0.6',
     description='Datahandling functions for bifrost (later to be API interface)',
     url='https://github.com/ssi-dk/bifrost/tree/master/lib/bifrostlib',
     author="Kim Ng, Martin Basterrechea",
     author_email="kimn@ssi.dk",
     packages=find_packages(),
-    install_requires=['ruamel.yaml', 'pymongo', 'pandas', 'python-magic']
+    install_requires=['ruamel.yaml', 'pymongo', 'pandas', 'numpy', 'python-magic-bin', 'dnspython']
     )
